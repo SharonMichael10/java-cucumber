4e530b41d735b9575d6097b42b7baf657f2dca30
SharonMichael10 <sharonsmichael10@gmail.com>
2024-09-30 19:17:08 -0700

    added dependencies to pom.xml write test for google

---
+++ src/test/java/support/DriverFactory.java
@@ -0,0 +1,24 @@
+package support;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    // private static final WebDriver driver = new ChromeDriver();
+
+    public static WebDriver getDriver() {
+        if (driver.get() == null) {
+            driver.set(new ChromeDriver());
+        }
+        return driver.get();
+    }
+
+    public static void closeDriver() {
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();
+        }
+    }
+}
